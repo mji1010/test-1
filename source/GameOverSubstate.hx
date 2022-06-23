d1abb0dc46bb9dec0bfcb2c17944c20b61cb29f5
mji1010 <105452317+mji1010@users.noreply.github.com>
2022-06-23 19:03:06 +0100

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -64,13 +64,6 @@ class GameOverSubstate extends MusicBeatSubstate
 
 		FlxG.sound.play(Paths.sound(deathSoundName));
 
-		#if android
-		if(ClientPrefs.vibration)
-		{
-			Hardware.vibrate(vibrationTime);
-		}
-		#end
-
 		Conductor.changeBPM(100);
 		// FlxG.camera.followLerp = 1;
 		// FlxG.camera.focusOn(FlxPoint.get(FlxG.width / 2, FlxG.height / 2));
